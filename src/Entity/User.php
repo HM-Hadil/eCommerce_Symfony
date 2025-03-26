659ad4afc900d916e84f2f298aa59794255c80e3
Hadil Hammami <hadilhammami670@gmail.com>
2025-03-27 00:23:41 +0100

    inscription viw

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,15 +1,17 @@
 <?php
-
 namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -17,16 +19,21 @@ class User
     private ?int $id = null;
 
     #[ORM\Column(length: 255, unique: true)]
+    #[Assert\NotBlank]
+    #[Assert\Email]
     private ?string $email = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $password = null;
 
-    #[ORM\Column]
-    private ?int $phoneNumber = null;
+    #[ORM\Column(length: 20)]
+    #[Assert\NotBlank]
+    #[Assert\Regex(pattern: "/^\+?[0-9]{10,15}$/", message: "Numéro de téléphone invalide")]
+    private ?string $phoneNumber = null;
 
     #[ORM\Column]
-    private ?bool $isVerified = null;
+    private ?bool $isVerified = false;
 
     #[ORM\ManyToMany(targetEntity: Role::class, inversedBy: "users")]
     #[ORM\JoinTable(name: "user_roles")]
@@ -35,6 +42,7 @@ class User
     public function __construct()
     {
         $this->roles = new ArrayCollection();
+        $this->isVerified = false;
     }
 
     public function getId(): ?int
@@ -64,12 +72,12 @@ class User
         return $this;
     }
 
-    public function getPhoneNumber(): ?int
+    public function getPhoneNumber(): ?string
     {
         return $this->phoneNumber;
     }
 
-    public function setPhoneNumber(int $phoneNumber): static
+    public function setPhoneNumber(string $phoneNumber): static
     {
         $this->phoneNumber = $phoneNumber;
         return $this;
@@ -86,9 +94,34 @@ class User
         return $this;
     }
 
-    public function getRoles(): Collection
+    // New methods for UserInterface
+    public function getUserIdentifier(): string
     {
-        return $this->roles;
+        return $this->email;
+    }
+
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+    }
+
+    public function getRoles(): array
+    {
+        // Convert Role objects to role names
+        $roleNames = $this->roles->map(function($role) {
+            return $role->getName();
+        })->toArray();
+
+        // Ensure every user has at least ROLE_USER
+        $roleNames[] = 'ROLE_USER';
+        
+        return array_unique($roleNames);
+    }
+
+    public function getSalt(): ?string
+    {
+        // Not needed for modern password hashing, return null
+        return null;
     }
 
     public function addRole(Role $role): static
@@ -96,7 +129,7 @@ class User
         if (!$this->roles->contains($role)) {
             $this->roles->add($role);
         }
-
+        
         return $this;
     }
 
